d3495d502f49978794f800f2f549a6871e3f6098
veiko911ira0903-art <veiko911ira0903@gmail.com>
2025-10-29 09:01:57 +0200

    Create create_players_map.h

---
+++ tracking players and points/create_players_map.h
@@ -0,0 +1,10 @@
+#ifndef PLAYERS_H
+#define PLAYERS_H
+
+#include <map>
+#include <string>
+#include <iostream> 
+
+std::map<std::string, int> create_players_map(int number_of_players);
+
+#endif // PLAYERS_H
