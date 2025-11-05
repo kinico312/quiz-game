0c283fdd91e654397284c51244a7b69b1c4cacbc
kinico <zetex312@gmail.com>
2025-11-05 02:42:20 +0200

    порядок в гите

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "head.hpp"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+
+    auto loaded_questions = dictionary();
+
+    if (loaded_questions.empty()) {
+        cout << "пустой!\n";
+        return 1;
+    }
+
+    cout << "загружено " << loaded_questions.size() << " вопросов!\n\n";
+
+
+    game();
+
+    return 0;
+}
